883e7cac53d657f5d05aec3a5e16abac707119e8
Ismailbulbul21 <ismailbulbul381@gmail.com>
2024-07-02 17:10:11 +0300

    footer

--- src/components/LeftFooter.jsx
+++ src/components/LeftFooter.jsx
@@ -10,7 +10,7 @@ const LeftFooter = () => {
 
 
 
-            <h1 className=' text-yellow-50 border-t-2 text-2xl font-bold'>@2023 copy right: <span className=' text-red-500'>Ismail Bulbul</span></h1>
+            <h1 className=' text-yellow-50 border-t-2 text-2xl font-bold'>@2024 copy right: <span className=' text-red-500'>Ismail Bulbul</span></h1>
 
 
    </div>
